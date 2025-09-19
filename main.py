08076be0b80665290bad4346a3ee533ef1d9e3c6
Ricardo <ricardofilho.rmsf6334@gmail.com>
2025-09-19 06:15:06 -0300

    mdspqpq

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from pathlib import Path  # Add this import
 import random
 
 topicos = ["Topico1", "Topico2", "Topico3","Topico4"]
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+static_path = Path(__file__).parent / "static"
+app.mount("/static", StaticFiles(directory=static_path), name="static")
 
-template = Jinja2Templates(directory="templates")
+index_file = Path(__file__).parent / "index.html"
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    
     return template.TemplateResponse(
         "home.html",
-        {"request": request, "nome": "Maurino"}
+        {"request": request, "name": "Maurino"}
     )
 
 @app.get("/topicos")
-def get_topico():
-    
+def get_topic():
     topico = random.choice(topicos)
-
     return {"topico": topico}
